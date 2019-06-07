028792ffb704b9335fa1bbbc4b71a3e77874497b
Myx-x <40513202+Myx-x@users.noreply.github.com>
2019-06-08 00:25:24 +0700

    Add files via upload

--- LCS Bottom Up.cpp
+++ LCS Bottom Up.cpp
@@ -23,9 +23,36 @@ int main()
     {
         for(int j=1;j<=strlen(arr_b);j++)
         {
-            if(arr_a[i-1]==arr_b[j-1]) memo[i][j]=memo[i-1][j-1]+1; //if same letter is detected, add up left diagonal with 1 (last character in both string)
+            if(arr_a[i-1]==arr_b[j-1]) memo[i][j]=memo[i-1][j-1]+1;  //if same letter is detected, add up left diagonal with 1 (last character in both string)
             else memo[i][j]=max(memo[i-1][j],memo[i][j-1]); //else just choose the current maximum lcs
         }
     }
-    printf("%d\n", memo[strlen(arr_a)][strlen(arr_b)]);
+
+    int longest = memo[strlen(arr_a)][strlen(arr_b)];
+    printf("%d\n", longest);
+
+    //Initialization
+    int pa=strlen(arr_a)-1;
+    int pb=strlen(arr_b)-1;
+
+    char res[100];
+    int p=0;
+
+    //Traverse Memo
+    while(pa!=-1 && pb!=-1)
+    {
+        if(arr_a[pa]==arr_b[pb]) // If char equal, traverse diagonally left upwards
+        {
+            res[p++]=arr_a[pa];
+            pa--;
+            pb--;
+        }
+        else if(memo[pa-1][pb]>memo[pa][pb-1]) pa--; // if going upwards is feasible, go upwards
+        else pb--; // if not, then traverse left
+    }
+
+    //Outputing result in reverse
+    if(longest==0) printf("LCS not found");
+    else for(int i=p-1;i>=0;i--) printf("%c", res[i]);
+    printf("\n");
 }
